12996c72c19e31c20fee1e465e80180959eff392
oraisdy <569450503@qq.com>
2017-07-02 17:46:38 +0800

    短信系统写完了

--- ShortMessage/src/main/hello/Application.java
+++ ShortMessage/src/main/hello/Application.java
@@ -19,8 +19,8 @@ import javax.jms.ConnectionFactory;
 public class Application {
 
     public static void main(String[] args) {
-        // Launch the application
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        System.out.println("=====短信系统开始运行=====");
 //        Sender sender = (Sender) ctx.getBean("sender");
 //        sender.sendMessage("15951641000", "AE010TX");
 
