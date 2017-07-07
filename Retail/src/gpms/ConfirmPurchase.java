f9dc61875ab5151bd8d55703ea0c397a3691c219
Camilla <1095279125728@tianchi.com>
2017-07-07 19:59:12 +0800

    。。。

--- Retail/src/gpms/ConfirmPurchase.java
+++ Retail/src/gpms/ConfirmPurchase.java
@@ -28,7 +28,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "confirmPurchase", propOrder = {
+@XmlType(name = "confirmPurchase2", propOrder = {
     "secretKey",
     "confirm"
 })
--- Retail/src/gpms/ConfirmPurchaseResponse.java
+++ Retail/src/gpms/ConfirmPurchaseResponse.java
@@ -26,7 +26,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "confirmPurchaseResponse", propOrder = {
+@XmlType(name = "confirmPurchaseResponse2", propOrder = {
     "result"
 })
 public class ConfirmPurchaseResponse {
